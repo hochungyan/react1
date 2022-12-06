461237bdbe3755e37af8214b839d99be4b57794d
Justin Ho <hochungyan@live.hk>
2022-12-06 06:35:54 +0000

    done

---
+++ src/Home/Footer/footer.js
@@ -0,0 +1,8 @@
+const Footer = ()=>{
+    return(
+  
+      "This is the Footer @2022"
+    )
+  }
+  
+  export default Footer;
---
+++ src/Home/home.js
@@ -0,0 +1,18 @@
+import Header from "./Header/header"
+import Footer from "./Footer/footer"
+import Body from "./Body/body"
+
+
+
+
+const Home=() =>
+{return(
+   <div>
+<Header/>
+<Body />
+<Footer />
+    </div>
+)
+
+}
+export default Home
